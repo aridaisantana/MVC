43e4b3594eae8e228b82f0ee2700657c45843af7
usuario <usuario@DESKTOP-I71O3FQ>
2020-01-23 20:08:42 +0000

    Loging taking the data from a data base with the MVC design pattern

---
+++ src/loginmvc/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginmvc;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author usuario
+ */
+public class Main extends JFrame{
+
+    
+    public static void main(String[] args) {
+        new Main().execute();
+    }
+
+    private void execute() {
+        this.setVisible(true);
+    }
+    
+}
